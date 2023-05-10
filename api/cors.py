de540763aa71d79909ac0fa0698ea84cd3a9b96e
Neel139 <np32978@gmail.com>
2023-05-10 17:59:18 +0530

    added cors.py

---
+++ api/cors.py
@@ -0,0 +1,6 @@
+from urllib import response
+
+class CorsMiddleware(object):
+    def process_response(self, req, resp):
+        response["Access-Control-Allow-Origin"] = "*"
+        return response
